4686554d029d75ba05bdd9b17189758886b61bfa
adarkond <akshatad1234@gmail.com>
2023-11-23 13:11:01 +0530

    "Simple Http Git Request"

--- src/main/java/com/Basic/microservices/controller/HelloWorldController.java
+++ src/main/java/com/Basic/microservices/controller/HelloWorldController.java
@@ -1,7 +1,14 @@
 package com.Basic.microservices.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+
+    @GetMapping("/hello-world")  //http GET Request.
+    public String helloWorld(){
+        return "Hello World";
+    }
 }
