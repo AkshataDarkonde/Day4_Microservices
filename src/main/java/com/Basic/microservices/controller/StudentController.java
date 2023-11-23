f247d4430900820cbe47bc492d3ec30ca46ee2d4
adarkond <akshatad1234@gmail.com>
2023-11-23 13:38:43 +0530

    "Student List with get Request "

--- src/main/java/com/Basic/microservices/controller/StudentController.java
+++ src/main/java/com/Basic/microservices/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.Basic.microservices.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -11,5 +14,14 @@ public class StudentController {
     public Student getStudent(){
         Student student= new Student(1, "Akshata","Darkonde");
         return student;
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1,"Radhika","Adhav"));
+        students.add(new Student(2,"Sham","Rathod"));
+        students.add(new Student(3,"Ram","Patil"));
+        return students;
     }
 }
