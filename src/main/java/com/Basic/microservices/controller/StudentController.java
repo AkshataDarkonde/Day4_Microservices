99428deb2d24ede4e4828e59746c359bbb4aa818
adarkond <akshatad1234@gmail.com>
2023-11-23 14:00:34 +0530

    "Student get Request with RequestParam "

--- src/main/java/com/Basic/microservices/controller/StudentController.java
+++ src/main/java/com/Basic/microservices/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.Basic.microservices.controller;
 import com.Basic.microservices.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -32,5 +33,10 @@ public class StudentController {
                                        @PathVariable("first-name")String firstNmae,
                                        @PathVariable("last-name")String lastName){
         return new Student(studentId,firstNmae,lastName);
+    }
+
+    @GetMapping("students/query")
+    public Student studentRequestVariable(@RequestParam int id, @RequestParam String firstName, @RequestParam String lastName){
+        return new Student(id,firstName,lastName);
     }
 }
